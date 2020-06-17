a2de9c6d89d947321e1f1526655b6c24cd599a2a
Pierre Barbera <pierre.pbdas@gmail.com>
2020-06-17 11:08:25 +0200

    updated eval scripts

--- evaluation/bv_eval.r
+++ evaluation/bv_eval.r
@@ -3,18 +3,26 @@
 library(Hmisc)
 library(readr)
 
-script.dir <- dirname( sys.frame(1)$ofile )
+# script.dir <- dirname( sys.frame(1)$ofile )
 base.dir <- file.path( getwd(), ".." )
 # base.dir <- file.path( script.dir, ".." )
-scrapp_measures <- read_csv( file.path( base.dir, "workdir", "scrapp", "result.csv" ) )
-place_measures <- read_csv( file.path( base.dir, "workdir", "bwpd", "result.csv" ) )
-guppy_measures <- read_csv( file.path( base.dir, "workdir", "guppy-fpd", "result.csv" ) )
+scrapp_measures <- read_csv( file.path( base.dir, "workdir", "bv", "scrapp", "result.csv" ) )
+names(scrapp_measures) <- c( names(scrapp_measures[,1]), paste(names(scrapp_measures[,-1]), "scrapp", sep = ".") )
+
+place_measures <- read_csv( file.path( base.dir, "workdir", "bv", "bwpd", "result.csv" ) )
+
+guppy_measures <- read_csv( file.path( base.dir, "workdir", "bv", "guppy-fpd_pendant", "result.csv" ) )
 colnames( guppy_measures ) <- paste( colnames( guppy_measures ), "guppy", sep=".")
 
-meta <- read_delim( file.path( base.dir, "datasets", "bv", "00_data", "meta_simple.csv" ), "\t" )
+otu_measures <- read_csv( file.path( base.dir, "workdir", "bv", "otu", "result.csv" ) )
+otu_measures <- otu_measures[, c("Shannon", "Simpson", "ACE", "Chao1", "sample")]
 
-measures <- merge( scrapp_measures, place_measures, by="sample", suffixes=c(".scrapp",".place") )
-measures <- merge( measures, guppy_measures, by.x="sample", by.y="placerun.guppy"  )
+meta <- read_delim( file.path( base.dir, "datasets", "bv", "data", "meta_simple.csv" ), "\t" )
+
+measures <- scrapp_measures
+# measures <- merge( measures, place_measures, by="sample", suffixes=c(".scrapp",".place") )
+measures <- merge( measures, guppy_measures, by.x="sample", by.y="placerun.guppy")
+measures <- merge( measures, otu_measures, by="sample")
 
 # get some order in the df
 library(dplyr)
@@ -33,8 +41,9 @@ measures_and_meta <- merge( measures, meta, by.x="sample", by.y="specimen" )
 
 require(ISLR)
 library(boot)
+library(data.table)
 
-log_reg_results <- data.frame(measure=character(0), accuracy.amsel=numeric(0), r_squared.nugent=numeric(0), stringsAsFactors = FALSE)
+log_reg_results <- data.frame(measure=character(0), accuracy.amsel=numeric(0), r_squared.nugent=numeric(0), amsel.p.value=numeric(0), stringsAsFactors = FALSE)
 for (m in colnames(measures[c(-1)])) {
   # log amsel model
   formula.log <- as.formula(sprintf("amsel ~ %s", m))
@@ -42,15 +51,33 @@ for (m in colnames(measures[c(-1)])) {
   model.log <- glm( formula.log, data = measures_and_meta, family = binomial)
   validation <- cv.glm( measures_and_meta, model.log )
   accuracy.amsel <- 1 - validation$delta[1]
+
   # lin nugent model
   formula.lin <- as.formula(sprintf("nugent ~ %s", m))
   print(formula.lin)
   model.lin <- lm( formula.lin, data = measures_and_meta )
   r_squared.nugent <- summary(model.lin)$r.squared
-  log_reg_results[nrow(log_reg_results) + 1,] = list(m, accuracy.amsel, r_squared.nugent)
-}
 
-glm.probs <- predict(model.log, measures_and_meta, type="response")
-glm.pred <- ifelse(glm.probs > 0.5, 1, 0)
-table(glm.pred,measures_and_meta$amsel)
-mean(glm.pred == measures_and_meta$amsel)
+  # two sample t-test
+  formula.t.test <- as.formula(sprintf("%s ~ amsel", m))
+  tt <- t.test(formula.t.test, data = measures_and_meta, var.equal = TRUE)
+
+  # log results
+  log_reg_results[nrow(log_reg_results) + 1,] = list(m, accuracy.amsel, r_squared.nugent, tt$p.value)
+}
+res_for_print <- log_reg_results[!(log_reg_results$measure %in% c("bwpd.guppy", "bwpd.scrapp")),]
+res_for_print$rank <- rowMeans( cbind(frank(-res_for_print$accuracy.amsel), frank(-res_for_print$r_squared.nugent), frank(res_for_print$amsel.p.value)) )
+res_for_print <- res_for_print[order(res_for_print$rank),]
+res_for_print <- format(res_for_print, digits=3)
+names(res_for_print) <- c("Measure", "Amsel accuracy", "Nugent $R^{2}$", "Amsel $p$-value", "Mean rank")
+
+
+out.path <- file.path( base.dir, "evaluation", "bv_pend" )
+if ( !dir.exists(out.path) ) {dir.create(out.path, recursive=TRUE)}
+write.csv(res_for_print, file.path( out.path, "result.csv" ), row.names = FALSE)
+
+
+# glm.probs <- predict(model.log, measures_and_meta, type="response")
+# glm.pred <- ifelse(glm.probs > 0.5, 1, 0)
+# table(glm.pred,measures_and_meta$amsel)
+# mean(glm.pred == measures_and_meta$amsel, na.rm = TRUE)
--- evaluation/eval.r
+++ evaluation/eval.r
@@ -12,12 +12,14 @@ base.dir <- file.path( getwd(), ".." )
 scrapp_measures <- read_csv( file.path( base.dir, "workdir", ds, "scrapp", "result.csv" ) )
 place_measures <- read_csv( file.path( base.dir, "workdir", ds, "bwpd", "result.csv" ) )
 guppy_measures <- read_csv( file.path( base.dir, "workdir", ds, "guppy-fpd", "result.csv" ) )
+# otu_measures <- read_csv( file.path( base.dir, "workdir", ds, "otu", "result.csv" ) )
 colnames( guppy_measures ) <- paste( colnames( guppy_measures ), "guppy", sep=".")
 
 # read, clean up the metadata table a bit to make it less unwieldy
 keep_names <- c("External ID", "diagnosis", "Subject has an acute gastrointestinal illness:", "Subject has an acute gastrointestinal infection or IBD:"
                 , "is_inflamed", "CRP (mg/L)", "VSL #3", "Blood in the stool")
-meta <- read_delim( file.path( base.dir, "datasets", ds, "data", "hmp2_metadata.csv" ), "," )[ ,keep_names]
+meta <- read_delim( file.path( base.dir, "datasets", ds, "data", "hmp2_metadata.csv" ), "," )
+# [ ,keep_names]
 names(meta) <- make.names( names(meta) )
 
 measures <- merge( scrapp_measures, place_measures, by="sample", suffixes=c(".scrapp",".place") )
@@ -39,54 +41,86 @@ ggdendrogram( clust, rotate = TRUE )
 
 # leave-one-out cross validation of linear regression of per-measure prediction of amsel criterion
 measures_and_meta <- merge( measures, meta, by.x="sample", by.y="External.ID" )
+measures_and_meta <- Filter(function(x)!all(is.na(x)), measures_and_meta)
 # add a binary IBD column
 measures_and_meta <- transform(measures_and_meta, ibd= ifelse(diagnosis=="nonIBD", as.integer(0), as.integer(1)))
 measures_and_meta <- transform(measures_and_meta, is_inflamed_bin= ifelse(is_inflamed=="No", as.integer(0), as.integer(1)))
+# measures_and_meta <- transform(measures_and_meta, uvsc= ifelse(diagnosis=="nonIBD", as.integer(0), as.integer(1)))
 
 require(ISLR)
 library(boot)
+library(stats)
+library(nnet)
+library(data.table)
+library(caret)
 
-results <- data.frame(measure=character(0),  accuracy.log=numeric(0), stringsAsFactors = FALSE)
-response <- "ibd"
+
+results <- data.frame(measure=character(0),  accuracy.log=numeric(0), ttest.pvalue=numeric(0), mn.acc=numeric(0), mn.pvalue=numeric(0), stringsAsFactors = FALSE)
 for (m in colnames(measures[c(-1)])) {
-  m<-"bwpd_0.place"
+  # m<-"bwpd_0.place"
   # log model
+  response <- "diagnosis"
   formula.log <- as.formula(sprintf("%s ~ %s", response, m))
   print(formula.log)
-  # rarefy
-  # measures_rarefied <- measures_and_meta %>% group_by( ibd ) %>% sample_n( min( table(unlist(measures_and_meta$ibd) ) ) )
-  
   measures_rarefied <- measures_and_meta %>% 
+    filter( diagnosis %in% c("UC", "CD", "nonIBD") ) %>%
+    mutate( diagnosis = ifelse( diagnosis=="nonIBD",as.integer(0), as.integer(1)) ) %>%
     group_by( .dots=response ) %>% 
-    sample_n( min( table(unlist(measures_and_meta[[response]]) ) ) )
+    sample_n( min( table(unlist(.[[response]]) ) ) )
     # glm( formula.log,  family=binomial, . )
   model.log <- glm( formula.log, data = measures_rarefied, family = binomial)
-  validation <- cv.glm( measures_rarefied, model.log, )
+  validation <- cv.glm( measures_rarefied, model.log )
   accuracy.log <- 1 - validation$delta[1]
-  results[nrow(results) + 1,] = list(m, accuracy.log)
-  break
-  # lin nugent model
-  # formula.lin <- as.formula(sprintf("CRP..mg.L. ~ %s", m))
+  
+  # ttest
+  formula.t.test <- as.formula(sprintf("%s ~ %s", m, response))
+  tt <-  measures_and_meta %>% 
+    mutate( diagnosis = ifelse( diagnosis=="nonIBD",as.integer(0), as.integer(1)) ) %>%
+    t.test(formula.t.test, data=., var.equal = TRUE)
+
+  # linear regression
+  # response <- "biopsy_location"
+  # formula.lin <- as.formula(sprintf("%s ~ %s", response, m))
   # print(formula.lin)
-  # model.lin <- lm( formula.lin, data = measures_and_meta )
+  # model.lin <- measures_and_meta %>%
+  #   mutate( biopsy_location := case_when(
+  #     biopsy_location=="Rectum" ~ 0,
+  #     biopsy_location=="Illeum" ~ 4,
+  #     biopsy_location=="Sigmoid Colon" ~ 1,
+  #     biopsy_location=="Descending (left-sided) colon" ~ 2,
+  #     biopsy_location=="Transverse colon" ~ 3,
+  #     TRUE ~ NA_real_
+  #   ) )%>%
+  #   lm( formula.lin, data=., na.action=na.exclude )
   # r_squared <- summary(model.lin)$r.squared
+  
+  
+  # kmeans clustering
+  # cl <- kmeans(measures_and_meta[m], 5)
+  # r_squared <- cl$betweenss / cl$totss
+  #
+  
+  # multinomial logistic regression
+  response <- "diagnosis"
+  mn.formula <- as.formula(sprintf("%s ~ %s", response, m))
+  print(mn.formula)
+  
+  set.seed(123)
+  train.smpls <- measures_and_meta[[response]] %>% 
+    createDataPartition(p = 0.8, list = FALSE)
+  train.data  <- measures_and_meta[train.smpls, ]
+  test.data <- measures_and_meta[-train.smpls, ]
+  
+  mn <- multinom(mn.formula, data = train.data)
+  mn.z <- summary(mn)$coefficients/summary(mn)$standard.errors
+  mn.pvalue <- (1 - pnorm(abs(mn.z), 0, 1)) * 2
+  pred <- mn %>% predict(test.data)
+  mn.acc <- mean(pred == test.data[[response]], na.rm = TRUE )
+  
+  results[nrow(results) + 1,] = list(m, accuracy.log, tt$p.value, mn.acc, mean(mn.pvalue[,m]) )
   # log_reg_results[nrow(log_reg_results) + 1,] = list(m, r_squared)
 }
-
-glm.probs <- predict(model.log, measures_and_meta, type="response")
-glm.pred <- ifelse(glm.probs > 0.5, 1, 0)
-table(glm.pred,measures_and_meta$ibd)
-mean(glm.pred == measures_and_meta$ibd)
+results$rank <- rowMeans( frank(-results$accuracy.log), frank(results$ttest.pvalue), frank(-mn.acc) )
 
 
-measures_diag <- measures_and_meta[, c("bwpd_0.scrapp", "diagnosis", "ibd")]
-ggplot(aes(ibd, bwpd_0.scrapp, color=diagnosis), data = measures_diag)+
-  # geom_point()+
-  geom_jitter(position = position_jitter(width = 0.05))+
-  scale_x_continuous(breaks=c(0,1),labels=c("No", "Yes"))
-
-
-ggplot(aes(is_inflamed_bin, bwpd_0.scrapp, color=is_inflamed), data = measures_and_meta[, c("bwpd_0.scrapp", "is_inflamed", "is_inflamed_bin")])+
-  geom_jitter(position = position_jitter(width = 0.05))+
-  scale_x_continuous(breaks=c(0,1),labels=c("No", "Yes"))
 
